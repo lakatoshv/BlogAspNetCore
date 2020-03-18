3bfb85ec646c2544bce314833e8a05f0dc6a49aa
Віталій Лакатош <some.pochta1@gmail.com>
2020-03-18 19:41:57 +0200

    Create database

--- Blog.Data/Migrations/20200311195726_Init.cs
+++ Blog.Data/Migrations/20200311195726_Init.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
-
-namespace Blog.Data.Migrations
+﻿namespace Blog.Data.Migrations
 {
+    using System;
+    using Microsoft.EntityFrameworkCore.Metadata;
+    using Microsoft.EntityFrameworkCore.Migrations;
+
     public partial class Init : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
