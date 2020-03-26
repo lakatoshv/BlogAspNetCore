8634917633627a576a2db9d1cf3288f43f00b238
Віталій Лакатош <some.pochta1@gmail.com>
2020-03-26 22:56:30 +0200

    Code refactoring and docblock in Data.Core

--- Blog.Data.Core/Entity.cs
+++ Blog.Data.Core/Entity.cs
@@ -1,58 +1,105 @@
-﻿namespace Blog.Data.Core
+﻿// <copyright file="Entity.cs" company="Blog">
+// Copyright (c) Blog. All rights reserved.
+// </copyright>
+
+namespace Blog.Data.Core
 {
     using System;
     using Blog.Core;
 
+    /// <summary>
+    /// Entity.
+    /// </summary>
     public abstract class Entity : IEntityBase<int>
     {
+        /// <summary>
+        /// Gets or sets id.
+        /// </summary>
         public int Id { get; set; }
 
-        private static bool IsTransient(Entity obj)
-        {
-            return obj != null && Equals(obj.Id, default(int));
-        }
-
-        private Type GetUnproxiedType()
-        {
-            return GetType();
-        }
-
-        public override bool Equals(object obj)
-        {
-            return Equals(obj as Entity);
-        }
-
-        public virtual bool Equals(Entity other)
-        {
-            if (other == null)
-                return false;
-
-            if (ReferenceEquals(this, other))
-                return true;
-
-            if (IsTransient(this) || IsTransient(other) || !Equals(Id, other.Id))
-                return false;
-
-            var otherType = other.GetUnproxiedType();
-            var thisType = GetUnproxiedType();
-
-            return thisType.IsAssignableFrom(otherType) || otherType.IsAssignableFrom(thisType);
-        }
-
-        public override int GetHashCode()
-        {
-            return Equals(Id, default(int)) ? base.GetHashCode() : Id.GetHashCode();
-        }
-
+        /// <summary>
+        /// Override ==.
+        /// </summary>
+        /// <param name="x">x.</param>
+        /// <param name="y">y.</param>
+        /// <returns>bool.</returns>
         public static bool operator ==(Entity x, Entity y)
         {
             return Equals(x, y);
         }
 
+        /// <summary>
+        /// Override !=.
+        /// </summary>
+        /// <param name="x">x.</param>
+        /// <param name="y">y.</param>
+        /// <returns>bool.</returns>
         public static bool operator !=(Entity x, Entity y)
         {
             return !(x == y);
         }
 
+        /// <summary>
+        /// Equals.
+        /// </summary>
+        /// <param name="obj">obj.</param>
+        /// <returns>bool.</returns>
+        public override bool Equals(object obj)
+        {
+            return this.Equals(obj as Entity);
+        }
+
+        /// <summary>
+        /// Equals.
+        /// </summary>
+        /// <param name="other">other.</param>
+        /// <returns>bool.</returns>
+        public virtual bool Equals(Entity other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            if (IsTransient(this) || IsTransient(other) || !Equals(this.Id, other.Id))
+            {
+                return false;
+            }
+
+            var otherType = other.GetUnproxiedType();
+            var thisType = this.GetUnproxiedType();
+
+            return thisType.IsAssignableFrom(otherType) || otherType.IsAssignableFrom(thisType);
+        }
+
+        /// <inheritdoc/>
+        public override int GetHashCode()
+        {
+            return Equals(this.Id, default(int)) ? base.GetHashCode() : this.Id.GetHashCode();
+        }
+
+        /// <summary>
+        /// Is transient.
+        /// </summary>
+        /// <param name="obj">obj.</param>
+        /// <returns>bool.</returns>
+        private static bool IsTransient(Entity obj)
+        {
+            return obj != null && Equals(obj.Id, default(int));
+        }
+
+        /// <summary>
+        /// Get unproxied type.
+        /// </summary>
+        /// <returns>Type.</returns>
+        private Type GetUnproxiedType()
+        {
+            return this.GetType();
+        }
     }
 }
