1e83eebd5f2a449c64930d0f0887162c453ad8d7
Віталій Лакатош <some.pochta1@gmail.com>
2020-12-24 11:29:19 +0200

    Create Integration tests project with simple getting post test

---
+++ Tests/Blog.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,26 @@
+
+using System.Net.Http;
+using System.Threading.Tasks;
+using Blog.Web;
+using Blog.Web.Contracts.V1;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace Blog.IntegrationTests
+{
+    public class UnitTest1
+    {
+        private readonly HttpClient _client;
+        public UnitTest1()
+        {
+            var appFactory = new WebApplicationFactory<Startup>();
+            _client = appFactory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Test1()
+        {
+            var post = await _client.GetAsync(ApiRoutes.PostsController.Show.Replace("{id}", "1")); 
+        }
+    }
+}
