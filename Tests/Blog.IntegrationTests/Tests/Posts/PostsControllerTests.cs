8059c6cc5f75ff09310fc0c2f55aa250e797b2e8
Віталій Лакатош <some.pochta1@gmail.com>
2020-12-29 00:06:08 +0200

    Create post test for empty response return

---
+++ Tests/Blog.IntegrationTests/Tests/Posts/PostsControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Blog.Data.Models;
+using Blog.IntegrationTests.MethodsForTests.Posts;
+using Blog.Web.Contracts.V1;
+using FluentAssertions;
+using Xunit;
+
+namespace Blog.IntegrationTests.Tests.Posts
+{
+    /// <summary>
+    /// Posts controller tests.
+    /// </summary>
+    /// <seealso cref="PostsTestsMethods" />
+    public class PostsControllerTests : PostsTestsMethods 
+    {
+        /// <summary>
+        /// Gets all without any posts returns empty response.
+        /// </summary>
+        [Fact]
+        public async Task GetAll_WithoutAnyPosts_ReturnsEmptyResponse()
+        {
+            // Arrange
+            await AuthenticateAsync();
+
+            // Act
+            var response = await TestClient.GetAsync(ApiRoutes.PostsController.GetPosts); 
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            (await response.Content.ReadAsAsync<List<Post>>()).Should().BeEmpty();
+        }
+    }
+}
