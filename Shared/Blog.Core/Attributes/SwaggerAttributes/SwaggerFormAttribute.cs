53429b0911ba01d1c7c69f4e0f2480663d2638d9
Віталій Лакатош <some.pochta1@gmail.com>
2021-03-18 23:54:45 +0200

    Add atributes

---
+++ Shared/Blog.Core/Attributes/SwaggerAttributes/SwaggerFormAttribute.cs
@@ -0,0 +1,48 @@
+﻿// <copyright file="SwaggerFormAttribute.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Blog.Core.Attributes.SwaggerAttributes
+{
+    using System;
+
+    /// <summary>
+    /// Custom attribute for Swagger Upload Form.
+    /// </summary>
+    /// <seealso cref="Attribute" />
+    [AttributeUsage(AttributeTargets.Method)]
+    public sealed class SwaggerFormAttribute : Attribute
+    {
+        /// <inheritdoc cref="Attribute"/>
+        public SwaggerFormAttribute(string parameterName, string description, bool hasFileUpload = true)
+        {
+            ParameterName = parameterName;
+            Description = description;
+            HasFileUpload = hasFileUpload;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether this instance has file upload.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if this instance has file upload; otherwise, <c>false</c>.
+        /// </value>
+        public bool HasFileUpload { get; }
+
+        /// <summary>
+        /// Gets the name of the parameter <see cref="T:IFormFile"/>.
+        /// </summary>
+        /// <value>
+        /// The name of the parameter.
+        /// </value>
+        public string ParameterName { get; }
+
+        /// <summary>
+        /// Gets the description.
+        /// </summary>
+        /// <value>
+        /// The description.
+        /// </value>
+        public string Description { get; }
+    }
+}
---
+++ Shared/Blog.Core/Attributes/ValidateModelStateAttribute.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="ValidateModelStateAttribute.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Blog.Core.Attributes
+{
+    using System.Linq;
+    using Blog.Core.Infrastructure.OperationResults;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Filters;
+
+    /// <summary>
+    /// Custom validation handler for availability to whit OperationResult.
+    /// </summary>
+    public class ValidateModelStateAttribute : ActionFilterAttribute
+    {
+        /// <inheritdoc cref="ActionNameAttribute"/>
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.ModelState.IsValid)
+            {
+                return;
+            }
+
+            var operation = OperationResult.CreateResult<object>();
+            var messages = context.ModelState.Values.SelectMany(x => x.Errors.Select(xx => xx.ErrorMessage));
+            var message = string.Join(" ", messages);
+            operation.AddError(message);
+            context.Result = new OkObjectResult(operation);
+        }
+    }
+}
