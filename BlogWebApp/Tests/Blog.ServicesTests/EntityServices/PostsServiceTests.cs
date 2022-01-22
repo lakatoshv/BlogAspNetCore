939c5638f9fae57e8dd9dd09fb9810a09bba7e62
Vitalii Lakatosh <some.pochta1@gmail.com>
2022-01-22 19:25:01 +0200

    Add find existing post test

---
+++ BlogWebApp/Tests/Blog.ServicesTests/EntityServices/PostsServiceTests.cs
@@ -0,0 +1,83 @@
+﻿using AutoMapper;
+using Blog.Data.Models;
+using Blog.Data.Repository;
+using Blog.Services;
+using Blog.Services.Interfaces;
+using Moq;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Blog.ServicesTests.EntityServices
+{
+    /// <summary>
+    /// Posts service tests.
+    /// </summary>
+    public class PostsServiceTests
+    {
+        /// <summary>
+        /// The posts service.
+        /// </summary>
+        private readonly IPostsService _postsService;
+
+        /// <summary>
+        /// The posts repository mock.
+        /// </summary>
+        private readonly Mock<IRepository<Post>> _postsRepositoryMock;
+
+        /// <summary>
+        /// The comments service mock.
+        /// </summary>
+        private readonly Mock<ICommentsService> _commentsServiceMock;
+
+        /// <summary>
+        /// The mapper mock.
+        /// </summary>
+        private readonly Mock<IMapper> _mapper;
+
+        /// <summary>
+        /// The posts tags relations service mock.
+        /// </summary>
+        private readonly Mock<IPostsTagsRelationsService> _postsTagsRelationsService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PostsServiceTests"/> class.
+        /// </summary>
+        public PostsServiceTests()
+        {
+            _postsRepositoryMock = new Mock<IRepository<Post>>();
+            _commentsServiceMock = new Mock<ICommentsService>();
+            _mapper = new Mock<IMapper>();
+            _postsTagsRelationsService = new Mock<IPostsTagsRelationsService>();
+            _postsService = new PostsService(_postsRepositoryMock.Object, _commentsServiceMock.Object, _mapper.Object, _postsTagsRelationsService.Object);
+        }
+
+        /// <summary>
+        /// Find post.
+        /// Should return post whenP post exists.
+        /// </summary>
+        [Fact]
+        public void Find_ShouldReturnPost_WhenPostExists()
+        {
+            //Arrange
+            var random = new Random();
+            var postId = random.Next(52);
+            var newPost = new Post
+            {
+                Id = postId,
+                Title = $"Created from ServicesTests {postId}",
+                Description = $"Created from ServicesTests {postId}",
+                Content = $"Created from ServicesTests {postId}",
+                ImageUrl = $"Created from ServicesTests {postId}",
+            };
+            _postsRepositoryMock.Setup(x => x.GetById(postId))
+                .Returns(newPost);
+
+            //Act
+            var post = _postsService.Find(postId);
+
+            //Assert
+            Assert.Equal(postId, post.Id);
+        }
+    }
+}
