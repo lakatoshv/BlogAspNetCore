a29500383fe5585ceb4054e8239690752e3225e6
Vitalii Lakatosh <some.pochta1@gmail.com>
2022-02-01 21:28:26 +0200

    Add get all posts by equal specification test

--- BlogWebApp/Tests/Blog.ServicesTests/EntityServices/PostsServiceTests.cs
+++ BlogWebApp/Tests/Blog.ServicesTests/EntityServices/PostsServiceTests.cs
@@ -224,6 +224,47 @@ namespace Blog.ServicesTests.EntityServices
         }
 
         /// <summary>
+        /// Get all posts with specification.
+        /// Should return post with equal specification when posts exists.
+        /// </summary>
+        /// <param name="equalCount">The equal count.</param>
+        /// <param name="titleSearch">The title search.</param>
+        [Theory]
+        [InlineData(1, "Created from ServicesTests 0")]
+        public void GetAllAsync_ShouldReturnPost_WithEqualsSpecification_WhenPostsExists(int equalCount, string titleSearch)
+        {
+            //Arrange
+            var random = new Random();
+            var postslist = new List<Post>();
+
+            for (var i = 0; i < random.Next(100); i++)
+            {
+                var postId = i;
+                postslist.Add(new Post
+                {
+                    Id = postId,
+                    Title = $"Created from ServicesTests {postId}",
+                    Description = $"Created from ServicesTests {postId}",
+                    Content = $"Created from ServicesTests {postId}",
+                    ImageUrl = $"Created from ServicesTests {postId}",
+                });
+            }
+
+
+            var specification = new PostSpecification(x => x.Title.Equals(titleSearch));
+            _postsRepositoryMock.Setup(x => x.GetAll(specification))
+                .Returns(() => postslist.Where(x => x.Title.Contains(titleSearch)).AsQueryable());
+
+            //Act
+            var posts = _postsService.GetAll(specification);
+
+            //Assert
+            Assert.NotNull(posts);
+            Assert.NotEmpty(posts);
+            Assert.Equal(equalCount, posts.ToList().Count);
+        }
+
+        /// <summary>
         /// Verify that function Find has been called.
         /// </summary>
         [Fact]
