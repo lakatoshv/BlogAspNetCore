6bf37785fa05c0194f61832557926f84e7ed5e98
Vitalii Lakatosh <some.pochta1@gmail.com>
2022-02-04 00:55:06 +0200

    Add async check if there are posts by equal specification test should return nothing

--- BlogWebApp/Tests/Blog.ServicesTests/EntityServices/PostsServiceTests.cs
+++ BlogWebApp/Tests/Blog.ServicesTests/EntityServices/PostsServiceTests.cs
@@ -1227,6 +1227,45 @@ namespace Blog.ServicesTests.EntityServices
             Assert.True(areAnyPosts);
         }
 
+        /// <summary>
+        /// Async check if there are any posts with specification.
+        /// Should return false with when posts does not exists.
+        /// </summary>
+        /// <param name="titleSearch">The title search.</param>
+        /// <returns>Task.</returns>
+        [Theory]
+        [InlineData("Created from ServicesTests -1")]
+        public async Task AnyAsync_ShouldReturnFalse_WithEqualSpecification_WhenPostsExists(string titleSearch)
+        {
+            //Arrange
+            var random = new Random();
+            var postslist = new List<Post>();
+
+            for (var i = 0; i < random.Next(100); i++)
+            {
+                var postId = i;
+                postslist.Add(new Post
+                {
+                    Id = postId,
+                    Title = $"Created from ServicesTests {postId}",
+                    Description = $"Created from ServicesTests {postId}",
+                    Content = $"Created from ServicesTests {postId}",
+                    ImageUrl = $"Created from ServicesTests {postId}",
+                });
+            }
+
+
+            var specification = new PostSpecification(x => x.Title.Equals(titleSearch));
+            _postsRepositoryMock.Setup(x => x.AnyAsync(specification))
+                .ReturnsAsync(() => postslist.Any(x => x.Title.Contains(titleSearch)));
+
+            //Act
+            var areAnyPosts = await _postsService.AnyAsync(specification);
+
+            //Assert
+            Assert.False(areAnyPosts);
+        }
+
         #endregion
 
         #region NotTestedYet
