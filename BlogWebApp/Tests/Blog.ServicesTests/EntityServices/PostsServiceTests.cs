87e03ce5ae4309ec0d9efc0ba9a432f169279cb6
Vitalii Lakatosh <some.pochta1@gmail.com>
2022-02-05 01:08:15 +0200

    Add get first or default test

--- BlogWebApp/Tests/Blog.ServicesTests/EntityServices/PostsServiceTests.cs
+++ BlogWebApp/Tests/Blog.ServicesTests/EntityServices/PostsServiceTests.cs
@@ -1328,6 +1328,46 @@ namespace Blog.ServicesTests.EntityServices
             _postsRepositoryMock.Verify(x => x.FirstOrDefault(specification), Times.Once);
         }
 
+        /// <summary>
+        /// Get first or default post with specification.
+        /// Should return post with contains specification when posts exists.
+        /// </summary>
+        /// <param name="titleSearch">The title search.</param>
+        [Theory]
+        [InlineData("Created from ServicesTests ")]
+        public void FirstOrDefault_ShouldReturnTrue_WithContainsSpecification_WhenPostsExists(string titleSearch)
+        {
+            //Test failed
+            //Arrange
+            var random = new Random();
+            var postslist = new List<Post>();
+
+            for (var i = 0; i < random.Next(100); i++)
+            {
+                var postId = i;
+                postslist.Add(new Post
+                {
+                    Id = postId,
+                    Title = $"Created from ServicesTests {postId}",
+                    Description = $"Created from ServicesTests {postId}",
+                    Content = $"Created from ServicesTests {postId}",
+                    ImageUrl = $"Created from ServicesTests {postId}",
+                });
+            }
+
+
+            var specification = new PostSpecification(x => x.Title.Contains(titleSearch));
+            _postsRepositoryMock.Setup(x => x.FirstOrDefault(specification))
+                .Returns(() => postslist.FirstOrDefault(x => x.Title.Contains(titleSearch)));
+
+            //Act
+            var post = _postsService.FirstOrDefault(specification);
+
+            //Assert
+            Assert.NotNull(post);
+            Assert.IsType<Post>(post);
+        }
+
         #endregion
 
         #region NotTestedYet
