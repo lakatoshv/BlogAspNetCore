3df0e8b7fd54fd18be2e211c7a194c4fcc8b9c5d
Vitalii Lakatosh <some.pochta1@gmail.com>
2022-02-06 01:24:09 +0200

    Add get last or default post test that shiuld return nothing

--- BlogWebApp/Tests/Blog.ServicesTests/EntityServices/PostsServiceTests.cs
+++ BlogWebApp/Tests/Blog.ServicesTests/EntityServices/PostsServiceTests.cs
@@ -1644,6 +1644,44 @@ namespace Blog.ServicesTests.EntityServices
             Assert.Null(post);
         }
 
+        /// <summary>
+        /// Get last or default post with specification.
+        /// Should return nothing with when posts does not exists.
+        /// </summary>
+        /// <param name="titleSearch">The title search.</param>
+        [Theory]
+        [InlineData("Created from ServicesTests 0")]
+        public void LastOrDefault_ShouldReturnNothing_WithEqualSpecification_WhenPostDoesNotExists(string titleSearch)
+        {
+            //Arrange
+            var random = new Random();
+            var postslist = new List<Post>();
+
+            for (var i = 0; i < random.Next(100); i++)
+            {
+                var postId = i;
+                postslist.Add(new Post
+                {
+                    Id = postId,
+                    Title = $"Created from ServicesTests {postId}",
+                    Description = $"Created from ServicesTests {postId}",
+                    Content = $"Created from ServicesTests {postId}",
+                    ImageUrl = $"Created from ServicesTests {postId}",
+                });
+            }
+
+
+            var specification = new PostSpecification(x => x.Title.Equals(titleSearch));
+            _postsRepositoryMock.Setup(x => x.LastOrDefault(specification))
+                .Returns(() => null);
+
+            //Act
+            var post = _postsService.LastOrDefault(specification);
+
+            //Assert
+            Assert.Null(post);
+        }
+
         #endregion
 
         #region NotTestedYet
