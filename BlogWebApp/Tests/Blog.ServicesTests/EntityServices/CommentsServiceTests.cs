01129ee01ccbee9f4c2e0710ce2f8cea649db9f5
Vitalii Lakatosh <some.pochta1@gmail.com>
2022-02-07 01:36:56 +0200

    Create comments service tests

---
+++ BlogWebApp/Tests/Blog.ServicesTests/EntityServices/CommentsServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using Blog.Data.Models;
+using Blog.Data.Repository;
+using Blog.Services;
+using Blog.Services.Interfaces;
+using Moq;
+
+namespace Blog.ServicesTests.EntityServices
+{
+    /// <summary>
+    /// Comments service tests.
+    /// </summary>
+    public class CommentsServiceTests
+    {
+        #region Fields
+
+        /// <summary>
+        /// The commnts service.
+        /// </summary>
+        private readonly ICommentsService _commentsService;
+
+        /// <summary>
+        /// The comments repository mock.
+        /// </summary>
+        private readonly Mock<IRepository<Comment>> _commentsRepositoryMock;
+
+        #endregion
+
+        #region Ctor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CommentsServiceTests"/> class.
+        /// </summary>
+        public CommentsServiceTests()
+        {
+            _commentsRepositoryMock = new Mock<IRepository<Comment>>();
+            _commentsService = new CommentsService(_commentsRepositoryMock.Object);
+        }
+
+        #endregion
+    }
+}
