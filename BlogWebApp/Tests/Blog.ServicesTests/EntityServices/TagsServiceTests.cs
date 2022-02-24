e0574248ff4dda1a7548c553caa69ae7b612f753
Vitalii Lakatosh <some.pochta1@gmail.com>
2022-02-24 23:19:22 +0200

    Add Get all tags with specification test

--- BlogWebApp/Tests/Blog.ServicesTests/EntityServices/TagsServiceTests.cs
+++ BlogWebApp/Tests/Blog.ServicesTests/EntityServices/TagsServiceTests.cs
@@ -241,6 +241,43 @@ namespace Blog.ServicesTests.EntityServices
             Assert.Equal(equalCount, tags.ToList().Count);
         }
 
+        /// <summary>
+        /// Get all comments with specification.
+        /// Should return nothing with  when tags does not exists.
+        /// </summary>
+        /// <param name="equalCount">The equal count.</param>
+        /// <param name="tagSearch">The tag search.</param>
+        [Theory]
+        [InlineData(0, "Tag -1")]
+        public void GetAll_ShouldReturnNothing_WithEqualSpecification_WhenTagsExists(int equalCount, string tagSearch)
+        {
+            //Arrange
+            var random = new Random();
+            var tagsList = new List<Tag>();
+
+            for (var i = 0; i < random.Next(100); i++)
+            {
+                tagsList.Add(new Tag
+                {
+                    Id = i,
+                    Title = $"Tag {i}",
+                });
+            }
+
+
+            var specification = new TagSpecification(x => x.Title.Equals(tagSearch));
+            _tagsRepositoryMock.Setup(x => x.GetAll(specification))
+                .Returns(() => tagsList.Where(x => x.Title.Contains(tagSearch)).AsQueryable());
+
+            //Act
+            var tags = _tagsService.GetAll(specification);
+
+            //Assert
+            Assert.NotNull(tags);
+            Assert.Empty(tags);
+            Assert.Equal(equalCount, tags.ToList().Count);
+        }
+
         #endregion
     }
 }
