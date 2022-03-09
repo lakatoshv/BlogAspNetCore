714af910cea879ab1c5ed68e80992445acc3de0d
Vitalii Lakatosh <some.pochta1@gmail.com>
2022-03-10 00:16:41 +0200

    Verify that function Get All with specification has been called

--- BlogWebApp/Tests/Blog.ServicesTests/EntityServices/PostTagRelationsServiceTests.cs
+++ BlogWebApp/Tests/Blog.ServicesTests/EntityServices/PostTagRelationsServiceTests.cs
@@ -9,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blog.Data.Specifications.Base;
 using Xunit;
 
 namespace Blog.ServicesTests.EntityServices
@@ -21,17 +22,17 @@ namespace Blog.ServicesTests.EntityServices
         #region Fields
 
         /// <summary>
-        /// The posts tags relations service.
+        /// The post tag relations tags relations service.
         /// </summary>
         private readonly IPostsTagsRelationsService _postsTagsRelationsService;
 
         /// <summary>
-        /// The posts tags relations repository mock.
+        /// The post tag relations tags relations repository mock.
         /// </summary>
         private readonly Mock<IRepository<PostsTagsRelations>> _postsTagsRelationsRepositoryMock;
 
         /// <summary>
-        /// The posts repository mock.
+        /// The post tag relations repository mock.
         /// </summary>
         private readonly Mock<IRepository<Post>> _postsRepositoryMock;
 
@@ -46,7 +47,7 @@ namespace Blog.ServicesTests.EntityServices
         private readonly Mock<IMapper> _mapper;
 
         /// <summary>
-        /// The posts service mock.
+        /// The post tag relations service mock.
         /// </summary>
         private readonly Mock<IPostsService> _postsServiceMock;
 
@@ -110,7 +111,7 @@ namespace Blog.ServicesTests.EntityServices
 
         /// <summary>
         /// Get all post tag relations.
-        /// Should return posts when post tag relations exists.
+        /// Should return post tag relations when post tag relations exists.
         /// </summary>
         /// <param name="notEqualCount">The not equal count.</param>
         [Theory]
@@ -146,7 +147,7 @@ namespace Blog.ServicesTests.EntityServices
 
         /// <summary>
         /// Get all post tag relations.
-        /// Should return posts when post tag relations exists.
+        /// Should return post tag relations when post tag relations exists.
         /// </summary>
         /// <param name="notEqualCount">The not equal count.</param>
         [Theory]
@@ -208,7 +209,7 @@ namespace Blog.ServicesTests.EntityServices
 
         /// <summary>
         /// Get all post tag relations.
-        /// Should return posts when post tag relations exists.
+        /// Should return post tag relations when post tag relations exists.
         /// </summary>
         /// <param name="notEqualCount">The not equal count.</param>
         [Theory]
@@ -278,6 +279,49 @@ namespace Blog.ServicesTests.EntityServices
 
             //Assert
             Assert.Empty(postsTagsRelations);
+        }
+
+        #endregion
+
+        #region Get all function With Specification
+
+        /// <summary>
+        /// Verify that function Get All with specification has been called.
+        /// </summary>
+        /// <param name="titleSearch">The title search.</param>
+        [Theory]
+        [InlineData("Tag ")]
+        public void Verify_FunctionGetAll_WithSpecification_HasBeenCalled(string titleSearch)
+        {
+            //Arrange
+            var random = new Random();
+            var postsTagsRelationsList = new List<PostsTagsRelations>();
+
+            for (var i = 0; i < random.Next(100); i++)
+            {
+                var tag = new Tag
+                {
+                    Id = i,
+                    Title = $"{titleSearch} {i}"
+                };
+                postsTagsRelationsList.Add(new PostsTagsRelations
+                {
+                    Id = i,
+                    PostId = i,
+                    TagId = i,
+                    Tag = tag
+                });
+            }
+
+            var specification = new BaseSpecification<PostsTagsRelations>(x => x.Tag.Title.Contains(titleSearch));
+            _postsTagsRelationsRepositoryMock.Setup(x => x.GetAll(specification))
+                .Returns(() => postsTagsRelationsList.Where(x => x.Tag.Title.Contains(titleSearch)).AsQueryable());
+
+            //Act
+            _postsTagsRelationsService.GetAll(specification);
+
+            //Assert
+            _postsTagsRelationsRepositoryMock.Verify(x => x.GetAll(specification), Times.Once);
         }
 
         #endregion
