ce906e233300d8ad4a9765ed4fa3f7e3e8289ff9
Vitalii Lakatosh <some.pochta1@gmail.com>
2022-03-09 10:41:20 +0200

    Create post tag relations service test

---
+++ BlogWebApp/Tests/Blog.ServicesTests/EntityServices/PostTagRelationsServiceTests.cs
@@ -0,0 +1,78 @@
+﻿using AutoMapper;
+using Blog.Data.Models;
+using Blog.Data.Repository;
+using Blog.Data.Specifications;
+using Blog.Services;
+using Blog.Services.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Blog.ServicesTests.EntityServices
+{
+    /// <summary>
+    /// Post tag relations service tests.
+    /// </summary>
+    public class PostTagRelationsServiceTests
+    {
+        #region Fields
+
+        /// <summary>
+        /// The posts tags relations service.
+        /// </summary>
+        private readonly IPostsTagsRelationsService _postsTagsRelationsService;
+
+        /// <summary>
+        /// The posts tags relations repository mock.
+        /// </summary>
+        private readonly Mock<IRepository<PostsTagsRelations>> _postsTagsRelationsRepositoryMock;
+
+        /// <summary>
+        /// The posts repository mock.
+        /// </summary>
+        private readonly Mock<IRepository<Post>> _postsRepositoryMock;
+
+        /// <summary>
+        /// The tags repository mock.
+        /// </summary>
+        private readonly Mock<IRepository<Tag>> _tagsRepositoryMock;
+
+        /// <summary>
+        /// The mapper mock.
+        /// </summary>
+        private readonly Mock<IMapper> _mapper;
+
+        /// <summary>
+        /// The posts service mock.
+        /// </summary>
+        private readonly Mock<PostsService> _postsServiceMock;
+
+        /// <summary>
+        /// The tags service mock.
+        /// </summary>
+        private readonly Mock<TagsService> _tagsServiceMock;
+
+        #endregion
+
+        #region Ctor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PostTagRelationsServiceTests"/> class.
+        /// </summary>
+        public PostTagRelationsServiceTests()
+        {
+            _postsServiceMock = new Mock<PostsService>();
+            _tagsServiceMock = new Mock<TagsService>();
+            _postsTagsRelationsRepositoryMock = new Mock<IRepository<PostsTagsRelations>>();
+            _postsRepositoryMock = new Mock<IRepository<Post>>();
+            _tagsRepositoryMock = new Mock<IRepository<Tag>>();
+            _mapper = new Mock<IMapper>();
+            _postsTagsRelationsService = new PostsTagsRelationsService(_postsTagsRelationsRepositoryMock.Object, _tagsServiceMock.Object);
+        }
+
+        #endregion
+    }
+}
