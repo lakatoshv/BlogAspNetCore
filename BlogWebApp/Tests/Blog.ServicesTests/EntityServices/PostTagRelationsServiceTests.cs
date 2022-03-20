b9729944130761e0c2579e2970810b3fe462dbcc
Vitalii Lakatosh <some.pochta1@gmail.com>
2022-03-21 00:27:55 +0200

    Add Get last or default post with specification test, should return nothing

--- BlogWebApp/Tests/Blog.ServicesTests/EntityServices/PostTagRelationsServiceTests.cs
+++ BlogWebApp/Tests/Blog.ServicesTests/EntityServices/PostTagRelationsServiceTests.cs
@@ -2393,6 +2393,47 @@ namespace Blog.ServicesTests.EntityServices
             Assert.Null(postsTagsRelations);
         }
 
+        /// <summary>
+        /// Get last or default post with specification.
+        /// Should return nothing with when post tag relations does not exists.
+        /// </summary>
+        /// <param name="titleSearch">The title search.</param>
+        [Theory]
+        [InlineData("Created from ServicesTests 0")]
+        public void LastOrDefault_ShouldReturnNothing_WithEqualSpecification_WhenPostTagRelationDoesNotExists(string titleSearch)
+        {
+            //Arrange
+            var random = new Random();
+            var postsTagsRelationsList = new List<PostsTagsRelations>();
+
+            for (var i = 0; i < random.Next(100); i++)
+            {
+                var tag = new Tag
+                {
+                    Id = i,
+                    Title = $"{titleSearch} {i}"
+                };
+                postsTagsRelationsList.Add(new PostsTagsRelations
+                {
+                    Id = i,
+                    PostId = i,
+                    TagId = i,
+                    Tag = tag
+                });
+            }
+
+
+            var specification = new BaseSpecification<PostsTagsRelations>(x => x.Tag.Title.Equals(titleSearch));
+            _postsTagsRelationsRepositoryMock.Setup(x => x.LastOrDefault(specification))
+                .Returns(() => null);
+
+            //Act
+            var postsTagsRelations = _postsTagsRelationsService.LastOrDefault(specification);
+
+            //Assert
+            Assert.Null(postsTagsRelations);
+        }
+
         #endregion
     }
 }
