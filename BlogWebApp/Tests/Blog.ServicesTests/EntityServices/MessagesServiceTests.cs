adb6086222baaeae3c4472c55cff2d4e3bcd2668
Vitalii Lakatosh <some.pochta1@gmail.com>
2022-03-30 00:19:44 +0300

    Add Get all messages with specification test

--- BlogWebApp/Tests/Blog.ServicesTests/EntityServices/MessagesServiceTests.cs
+++ BlogWebApp/Tests/Blog.ServicesTests/EntityServices/MessagesServiceTests.cs
@@ -289,6 +289,65 @@ namespace Blog.ServicesTests.EntityServices
             Assert.NotEqual(notEqualCount, messages.ToList().Count);
         }
 
+
+        /// <summary>
+        /// Get all messages with specification.
+        /// Should return message with equal specification when messages exists.
+        /// </summary>
+        /// <param name="equalCount">The equal count.</param>
+        /// <param name="bodySearch">The body search.</param>
+        [Theory]
+        [InlineData(1, "Test body0")]
+        public void GetAll_ShouldReturnMessage_WithEqualsSpecification_WhenMessagesExists(int equalCount, string bodySearch)
+        {
+            //Arrange
+            var random = new Random();
+            var messagesList = new List<Message>();
+
+            var sender = new ApplicationUser
+            {
+                Id = new Guid().ToString(),
+                FirstName = "Test fn",
+                LastName = "Test ln",
+                Email = "test@test.test",
+                UserName = "test@test.test"
+            };
+
+            for (var i = 0; i < random.Next(100); i++)
+            {
+                var recipient = new ApplicationUser
+                {
+                    Id = new Guid().ToString(),
+                    FirstName = $"Test fn{i}",
+                    LastName = $"Test ln{i}",
+                    Email = $"test{i}@test.test",
+                    UserName = $"test{i}@test.test"
+                };
+                messagesList.Add(new Message
+                {
+                    Id = i,
+                    SenderId = sender.Id,
+                    Sender = sender,
+                    RecipientId = recipient.Id,
+                    Recipient = recipient,
+                    Subject = $"Test subject{i}",
+                    Body = $"Test body{i}"
+                });
+            }
+
+            var specification = new MessageSpecification(x => x.Body.Contains(bodySearch));
+            _messagesRepositoryMock.Setup(x => x.GetAll(specification))
+                .Returns(() => messagesList.Where(x => x.Body.Contains(bodySearch)).AsQueryable());
+
+            //Act
+            var messages = _messagesService.GetAll(specification);
+
+            //Assert
+            Assert.NotNull(messages);
+            Assert.NotEmpty(messages);
+            Assert.Equal(equalCount, messages.ToList().Count);
+        }
+
         #endregion
     }
 }
