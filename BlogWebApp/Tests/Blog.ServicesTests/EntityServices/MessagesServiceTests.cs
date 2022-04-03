9acf8fcbb7fe6d9135b8e49bd57fef108e3429ff
Vitalii Lakatosh <some.pochta1@gmail.com>
2022-04-03 18:29:26 +0300

    Verify that function Insert Async has been called

--- BlogWebApp/Tests/Blog.ServicesTests/EntityServices/MessagesServiceTests.cs
+++ BlogWebApp/Tests/Blog.ServicesTests/EntityServices/MessagesServiceTests.cs
@@ -774,5 +774,59 @@ namespace Blog.ServicesTests.EntityServices
         }
 
         #endregion
+
+        #region Insert Async function
+
+        /// <summary>
+        /// Verify that function Insert Async has been called.
+        /// </summary>
+        [Fact]
+        public async Task Verify_FunctionInsertAsync_HasBeenCalled()
+        {
+            //Arrange
+            var random = new Random();
+            var messageId = random.Next(52);
+
+            var sender = new ApplicationUser
+            {
+                Id = new Guid().ToString(),
+                FirstName = "Test fn",
+                LastName = "Test ln",
+                Email = "test@test.test",
+                UserName = "test@test.test"
+            };
+
+            var recipient = new ApplicationUser
+            {
+                Id = new Guid().ToString(),
+                FirstName = $"Test fn{messageId}",
+                LastName = $"Test ln{messageId}",
+                Email = $"test{messageId}@test.test",
+                UserName = $"test{messageId}@test.test"
+            };
+            var newMessage = new Message
+            {
+                SenderId = sender.Id,
+                Sender = sender,
+                RecipientId = recipient.Id,
+                Recipient = recipient,
+                Subject = $"Test subject{messageId}",
+                Body = $"Test body{messageId}"
+            };
+
+            _messagesRepositoryMock.Setup(x => x.InsertAsync(newMessage))
+                .Callback(() =>
+                {
+                    newMessage.Id = messageId;
+                });
+
+            //Act
+            await _messagesService.InsertAsync(newMessage);
+
+            //Assert
+            _messagesRepositoryMock.Verify(x => x.InsertAsync(newMessage), Times.Once);
+        }
+
+        #endregion
     }
 }
