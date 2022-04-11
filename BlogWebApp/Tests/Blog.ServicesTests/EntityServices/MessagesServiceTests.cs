78454b830900b5fd029db83f6b824db77c49b957
Vitalii Lakatosh <some.pochta1@gmail.com>
2022-04-11 18:54:44 +0300

    Verify that function Delete has been called

--- BlogWebApp/Tests/Blog.ServicesTests/EntityServices/MessagesServiceTests.cs
+++ BlogWebApp/Tests/Blog.ServicesTests/EntityServices/MessagesServiceTests.cs
@@ -1121,5 +1121,66 @@ namespace Blog.ServicesTests.EntityServices
         }
 
         #endregion
+
+        #region Delete function
+
+        /// <summary>
+        /// Verify that function Delete has been called.
+        /// </summary>
+        [Fact]
+        public void Verify_FunctionDelete_HasBeenCalled()
+        {
+            //Arrange
+            var random = new Random();
+            var messageId = random.Next(52);
+
+            var sender = new ApplicationUser
+            {
+                Id = new Guid().ToString(),
+                FirstName = "Test fn",
+                LastName = "Test ln",
+                Email = "test@test.test",
+                UserName = "test@test.test"
+            };
+
+            var recipient = new ApplicationUser
+            {
+                Id = new Guid().ToString(),
+                FirstName = $"Test fn{messageId}",
+                LastName = $"Test ln{messageId}",
+                Email = $"test{messageId}@test.test",
+                UserName = $"test{messageId}@test.test"
+            };
+            var newMessage = new Message
+            {
+                Id = messageId,
+                SenderId = sender.Id,
+                Sender = sender,
+                RecipientId = recipient.Id,
+                Recipient = recipient,
+                Subject = $"Test subject{messageId}",
+                Body = $"Test body{messageId}"
+            };
+            _messagesRepositoryMock.Setup(x => x.Insert(newMessage))
+                .Callback(() =>
+                {
+                    newMessage.Id = messageId;
+                });
+            _messagesRepositoryMock.Setup(x => x.GetById(messageId))
+                .Returns(() => newMessage);
+
+            //Act
+            _messagesService.Insert(newMessage);
+            var tag = _messagesService.Find(messageId);
+            _messagesService.Delete(tag);
+            _messagesRepositoryMock.Setup(x => x.GetById(messageId))
+                .Returns(() => null);
+            _messagesService.Find(messageId);
+
+            //Assert
+            _messagesRepositoryMock.Verify(x => x.Delete(tag), Times.Once);
+        }
+
+        #endregion
     }
 }
