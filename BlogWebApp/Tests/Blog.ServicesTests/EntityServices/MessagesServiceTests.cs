4ea244b6336b40b55f74801f28eec8c31d4df940
Vitalii Lakatosh <some.pochta1@gmail.com>
2022-04-12 19:01:06 +0300

    Add Async delete message test

--- BlogWebApp/Tests/Blog.ServicesTests/EntityServices/MessagesServiceTests.cs
+++ BlogWebApp/Tests/Blog.ServicesTests/EntityServices/MessagesServiceTests.cs
@@ -1297,6 +1297,65 @@ namespace Blog.ServicesTests.EntityServices
             _messagesRepositoryMock.Verify(x => x.DeleteAsync(comment), Times.Once);
         }
 
+        /// <summary>
+        /// Async delete message.
+        /// Should return nothing when message is deleted.
+        /// </summary>
+        /// <returns>Task.</returns>
+        [Fact]
+        public async Task DeleteAsync_ShouldReturnNothing_WhenMessageIsDeleted()
+        {
+            //Arrange
+            var random = new Random();
+            var messageId = random.Next(52);
+
+            var sender = new ApplicationUser
+            {
+                Id = new Guid().ToString(),
+                FirstName = "Test fn",
+                LastName = "Test ln",
+                Email = "test@test.test",
+                UserName = "test@test.test"
+            };
+
+            var recipient = new ApplicationUser
+            {
+                Id = new Guid().ToString(),
+                FirstName = $"Test fn{messageId}",
+                LastName = $"Test ln{messageId}",
+                Email = $"test{messageId}@test.test",
+                UserName = $"test{messageId}@test.test"
+            };
+            var newMessage = new Message
+            {
+                SenderId = sender.Id,
+                Sender = sender,
+                RecipientId = recipient.Id,
+                Recipient = recipient,
+                Subject = $"Test subject{messageId}",
+                Body = $"Test body{messageId}"
+            };
+
+            _messagesRepositoryMock.Setup(x => x.InsertAsync(newMessage))
+                .Callback(() =>
+                {
+                    newMessage.Id = messageId;
+                });
+            _messagesRepositoryMock.Setup(x => x.GetByIdAsync(messageId))
+                .ReturnsAsync(() => newMessage);
+
+            //Act
+            await _messagesService.InsertAsync(newMessage);
+            var message = await _messagesService.FindAsync(messageId);
+            await _messagesService.DeleteAsync(message);
+            _messagesRepositoryMock.Setup(x => x.GetByIdAsync(messageId))
+                .ReturnsAsync(() => null);
+            var deletedMessage = await _messagesService.FindAsync(messageId);
+
+            //Assert
+            Assert.Null(deletedMessage);
+        }
+
         #endregion
     }
 }
