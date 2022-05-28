f32345917d1565f2a7328ce15747e5445c0f7f27
Vitalii Lakatosh <some.pochta1@gmail.com>
2022-05-28 16:44:11 +0300

    Get posts from Api

---
+++ Clients/BlogBlazor/BlogBlazor/Services/Interfaces/IPostsService.cs
@@ -0,0 +1,19 @@
+﻿using Blazor.Contracts.V1.Requests.PostsRequests;
+using Blazor.Contracts.V1.Responses.PostsResponses;
+using System.Threading.Tasks;
+
+namespace BlogBlazor.Services.Interfaces
+{
+    /// <summary>
+    /// Posts service interface.
+    /// </summary>
+    public interface IPostsService
+    {
+        /// <summary>
+        /// Gets the posts.
+        /// </summary>
+        /// <param name="searchParameters">The search parameters.</param>
+        /// <returns>Task.</returns>
+        Task<PagedPostsResponse> GetPosts(PostsSearchParametersRequest searchParameters);
+    }
+}
---
+++ Clients/BlogBlazor/BlogBlazor/Services/PostsService.cs
@@ -0,0 +1,32 @@
+﻿namespace BlogBlazor.Services
+{
+    using Blazor.Contracts.V1.Requests.PostsRequests;
+    using Blazor.Contracts.V1.Responses.PostsResponses;
+    using Blazor.Sdk.V1;
+    using BlogBlazor.Constants;
+    using Interfaces;
+    using Refit;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Posts service.
+    /// </summary>
+    /// <seealso cref="IPostsService" />
+    public class PostsService : IPostsService
+    {
+        /// <summary>
+        /// The posts controller requests API.
+        /// </summary>
+        private readonly IPostsControllerRequestsApi _postsControllerRequestsApi;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PostsService"/> class.
+        /// </summary>
+        public PostsService()
+            => _postsControllerRequestsApi = RestService.For<IPostsControllerRequestsApi>(ApiUrls.BlogApiUrl);
+
+        /// <inheritdoc cref="IPostsService"/>
+        public async Task<PagedPostsResponse> GetPosts(PostsSearchParametersRequest searchParameters)
+            => await _postsControllerRequestsApi.GetPosts(searchParameters);
+    }
+}
