5b9c8e0748260bcb71fed0c40c789bf97fff8d4d
Vitalii Lakatosh <some.pochta1@gmail.com>
2022-06-23 15:42:38 +0300

    Add Delete Async by enumerable messages tests

--- BlogWebApp/Tests/Blog.ServicesTests/EntityServices/MessagesServiceTests.cs
+++ BlogWebApp/Tests/Blog.ServicesTests/EntityServices/MessagesServiceTests.cs
@@ -1895,6 +1895,141 @@ namespace Blog.ServicesTests.EntityServices
 
         #endregion
 
+        #region Delete By Enumerable function
+
+        /// <summary>
+        /// Verify that function Delete By Enumerable has been called.
+        /// </summary>
+        [Fact]
+        public void Verify_FunctionDeleteByEnumerable_HasBeenCalled()
+        {
+            //Arrange
+            var random = new Random();
+            var messageId = random.Next(52);
+            var itemsCount = random.Next(10);
+            var newMessages = new List<Message>();
+
+            var sender = new ApplicationUser
+            {
+                Id = new Guid().ToString(),
+                FirstName = "Test fn",
+                LastName = "Test ln",
+                Email = "test@test.test",
+                UserName = "test@test.test"
+            };
+
+            var recipient = new ApplicationUser
+            {
+                Id = new Guid().ToString(),
+                FirstName = $"Test fn{messageId}",
+                LastName = $"Test ln{messageId}",
+                Email = $"test{messageId}@test.test",
+                UserName = $"test{messageId}@test.test"
+            };
+
+            for (int i = 0; i < itemsCount; i++)
+            {
+                newMessages.Add(new Message
+                {
+                    SenderId = sender.Id,
+                    Sender = sender,
+                    RecipientId = recipient.Id,
+                    Recipient = recipient,
+                    Subject = $"Test subject{messageId}",
+                    Body = $"Test body{messageId}"
+                });
+            }
+
+            _messagesRepositoryMock.Setup(x => x.Insert(newMessages))
+                .Callback(() =>
+                {
+                    for (var i = 0; i < itemsCount; i++)
+                    {
+                        newMessages[i].Id = messageId + i;
+                    }
+                });
+            _messagesRepositoryMock.Setup(x => x.Delete(newMessages))
+                .Callback(() =>
+                {
+                    newMessages = null;
+                });
+
+            //Act
+            _messagesService.Insert(newMessages);
+            _messagesService.Delete(newMessages);
+
+            //Assert
+            _messagesRepositoryMock.Verify(x => x.Delete(newMessages), Times.Once);
+        }
+
+        /// <summary>
+        /// Delete By Enumerable message.
+        /// Should return nothing when message is deleted.
+        /// </summary>
+        [Fact]
+        public void DeleteByEnumerable_ShouldReturnNothing_WhenMessageDeleted()
+        {
+            //Arrange
+            var random = new Random();
+            var messageId = random.Next(52);
+            var itemsCount = random.Next(10);
+            var newMessages = new List<Message>();
+
+            var sender = new ApplicationUser
+            {
+                Id = new Guid().ToString(),
+                FirstName = "Test fn",
+                LastName = "Test ln",
+                Email = "test@test.test",
+                UserName = "test@test.test"
+            };
+
+            var recipient = new ApplicationUser
+            {
+                Id = new Guid().ToString(),
+                FirstName = $"Test fn{messageId}",
+                LastName = $"Test ln{messageId}",
+                Email = $"test{messageId}@test.test",
+                UserName = $"test{messageId}@test.test"
+            };
+
+            for (int i = 0; i < itemsCount; i++)
+            {
+                newMessages.Add(new Message
+                {
+                    SenderId = sender.Id,
+                    Sender = sender,
+                    RecipientId = recipient.Id,
+                    Recipient = recipient,
+                    Subject = $"Test subject{messageId}",
+                    Body = $"Test body{messageId}"
+                });
+            }
+
+            _messagesRepositoryMock.Setup(x => x.Insert(newMessages))
+                .Callback(() =>
+                {
+                    for (var i = 0; i < itemsCount; i++)
+                    {
+                        newMessages[i].Id = messageId + i;
+                    }
+                });
+            _messagesRepositoryMock.Setup(x => x.Delete(newMessages))
+                .Callback(() =>
+                {
+                    newMessages = null;
+                });
+
+            //Act
+            _messagesService.Insert(newMessages);
+            _messagesService.Delete(newMessages);
+
+            //Assert
+            Assert.Null(newMessages);
+        }
+
+        #endregion
+
         #region Delete Async By Id function
 
         /// <summary>
