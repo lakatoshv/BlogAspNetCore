0c64fae30f0be5dc3bc76d3727277dddf5d00835
Vitalii Lakatosh <some.pochta1@gmail.com>
2023-08-14 12:26:29 +0300

    Edit post from admin panel

--- BlogWebApp/Blog.Web/ClientApp/src/app/admin-portal/admin-posts/admin-posts-routing.module.ts
+++ BlogWebApp/Blog.Web/ClientApp/src/app/admin-portal/admin-posts/admin-posts-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminPostsListComponent } from './admin-posts-list/admin-posts-list.component';
 import { ShowComponent } from 'src/app/shared/posts/show/show.component';
 import { AddPostComponent } from 'src/app/shared/posts/add-post/add-post.component';
+import { EditPostComponent } from 'src/app/shared/posts/edit-post/edit-post.component';
 
 const routes: Routes = [
   {
@@ -16,6 +17,10 @@ const routes: Routes = [
   {
     path: 'posts/add',
     component: AddPostComponent
+  },
+  {
+    path: 'posts/edit/:post-id',
+    component: EditPostComponent
   },
 ];
 
