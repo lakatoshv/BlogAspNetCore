451c339ab9a197042a0e44c483799f5647766ebc
Vitalii Lakatosh <some.pochta1@gmail.com>
2023-10-14 10:12:00 +0300

    Code refactoring in Blazor.Sdk

--- Clients/BlogBlazor/Blazor.Sdk/V1/IAccountsControllerRequestsApi.cs
+++ Clients/BlogBlazor/Blazor.Sdk/V1/IAccountsControllerRequestsApi.cs
@@ -1,72 +1,70 @@
-﻿
-namespace Blazor.Sdk.V1
+﻿namespace Blazor.Sdk.V1;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Blazor.Contracts.V1.Requests.UsersRequests;
+using Blazor.Contracts.V1.Responses.UsersResponses;
+using Microsoft.AspNetCore.Identity;
+using Refit;
+
+/// <summary>
+/// Account controller requests api interface.
+/// </summary>
+public interface IAccountsControllerRequestsApi
 {
-    using System.Collections.Generic;
-    using System.Threading.Tasks;
-    using Blazor.Contracts.V1.Requests.UsersRequests;
-    using Blazor.Contracts.V1.Responses.UsersResponses;
-    using Microsoft.AspNetCore.Identity;
-    using Refit;
+    /// <summary>
+    /// Initializes the specified user identifier.
+    /// </summary>
+    /// <param name="userId">The user identifier.</param>
+    /// <returns>Task.</returns>
+    /// <response code="200">Get user data by user id.</response>
+    /// <response code="400">Unable to get user data by user id.</response>
+    [Get("/api/v1/accounts/initialize/{userId}")]
+    Task<List<RoleResponse>> Initialize(string userId);
 
     /// <summary>
-    /// Account controller requests api interface.
+    /// Gets all users.
     /// </summary>
-    public interface IAccountsControllerRequestsApi
-    {
-        /// <summary>
-        /// Initializes the specified user identifier.
-        /// </summary>
-        /// <param name="userId">The user identifier.</param>
-        /// <returns>Task.</returns>
-        /// <response code="200">Get user data by user id.</response>
-        /// <response code="400">Unable to get user data by user id.</response>
-        [Get("/api/v1/accounts/initialize/{userId}")]
-        Task<List<RoleResponse>> Initialize(string userId);
+    /// <returns>Task.</returns>
+    /// <response code="204">No content.</response>
+    [Get("/api/v1/accounts/get-all-users")]
+    Task<List<RoleResponse>> GetAllUsers();
 
-        /// <summary>
-        /// Gets all users.
-        /// </summary>
-        /// <returns>Task.</returns>
-        /// <response code="204">No content.</response>
-        [Get("/api/v1/accounts/get-all-users")]
-        Task<List<RoleResponse>> GetAllUsers();
+    /// <summary>
+    /// Sends the verification email asynchronous.
+    /// </summary>
+    /// <returns>Task.</returns>
+    [Get("/api/v1/accounts/send-confirmation-email")]
+    Task SendVerificationEmailAsync();
 
-        /// <summary>
-        /// Sends the verification email asynchronous.
-        /// </summary>
-        /// <returns>Task.</returns>
-        [Get("/api/v1/accounts/send-confirmation-email")]
-        Task SendVerificationEmailAsync();
-
-        /// <summary>
-        /// Posts the asynchronous.
-        /// </summary>
-        /// <param name="credentials">The credentials.</param>
-        /// <returns>Task.</returns>
-        /// <response code="200">User login.</response>
-        /// <response code="400">Unable to user login.</response>
-        [Post("/api/v1/accounts/login")]
-        Task<string> PostAsync(LoginRequest credentials);
+    /// <summary>
+    /// Posts the asynchronous.
+    /// </summary>
+    /// <param name="credentials">The credentials.</param>
+    /// <returns>Task.</returns>
+    /// <response code="200">User login.</response>
+    /// <response code="400">Unable to user login.</response>
+    [Post("/api/v1/accounts/login")]
+    Task<string> PostAsync(LoginRequest credentials);
 
 
-        /// <summary>
-        /// Creates the asynchronous.
-        /// </summary>
-        /// <param name="model">The model.</param>
-        /// <returns>Task.</returns>
-        /// <response code="201">Register user.</response>
-        /// <response code="400">Unable to register user.</response>
-        [Post("/api/v1/accounts/register")]
-        Task<IdentityResult> CreateAsync(RegistrationRequest model);
+    /// <summary>
+    /// Creates the asynchronous.
+    /// </summary>
+    /// <param name="model">The model.</param>
+    /// <returns>Task.</returns>
+    /// <response code="201">Register user.</response>
+    /// <response code="400">Unable to register user.</response>
+    [Post("/api/v1/accounts/register")]
+    Task<IdentityResult> CreateAsync(RegistrationRequest model);
 
-        /// <summary>
-        /// Updates the asynchronous.
-        /// </summary>
-        /// <param name="model">The model.</param>
-        /// <returns>Task.</returns>
-        /// <response code="204">Change password.</response>
-        /// <response code="400">Unable to change password.</response>
-        [Put("/api/v1/accounts/change-password")]
-        Task UpdateAsync(ChangePasswordRequest model);
-    }
+    /// <summary>
+    /// Updates the asynchronous.
+    /// </summary>
+    /// <param name="model">The model.</param>
+    /// <returns>Task.</returns>
+    /// <response code="204">Change password.</response>
+    /// <response code="400">Unable to change password.</response>
+    [Put("/api/v1/accounts/change-password")]
+    Task UpdateAsync(ChangePasswordRequest model);
 }
--- Clients/BlogBlazor/Blazor.Sdk/V1/ICommentsControllerRequestsApi.cs
+++ Clients/BlogBlazor/Blazor.Sdk/V1/ICommentsControllerRequestsApi.cs
@@ -1,90 +1,89 @@
-﻿namespace Blazor.Sdk.V1
+﻿namespace Blazor.Sdk.V1;
+
+using Blazor.Contracts.V1.Requests;
+using Blazor.Contracts.V1.Requests.CommentsRequests;
+using Blazor.Contracts.V1.Responses;
+using Blazor.Contracts.V1.Responses.CommentsResponses;
+using Refit;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Comments controller requests api interface.
+/// </summary>
+public interface ICommentsControllerRequestsApi
 {
-    using Blazor.Contracts.V1.Requests;
-    using Blazor.Contracts.V1.Requests.CommentsRequests;
-    using Blazor.Contracts.V1.Responses;
-    using Blazor.Contracts.V1.Responses.CommentsResponses;
-    using Refit;
-    using System.Collections.Generic;
-    using System.Threading.Tasks;
+    /// <summary>
+    /// Gets all comments.
+    /// </summary>
+    /// <returns>Task.</returns>
+    /// <response code="200">Get all comments.</response>
+    [Get("/api/v1/comments")]
+    Task<List<CommentResponse>> GetAllComments();
 
     /// <summary>
-    /// Comments controller requests api interface.
+    /// Gets the comments.
     /// </summary>
-    public interface ICommentsControllerRequestsApi
-    {
-        /// <summary>
-        /// Gets all comments.
-        /// </summary>
-        /// <returns>Task.</returns>
-        /// <response code="200">Get all comments.</response>
-        [Get("/api/v1/comments")]
-        Task<List<CommentResponse>> GetAllComments();
+    /// <param name="sortParameters">The sort parameters.</param>
+    /// <returns>Task.</returns>
+    /// <response code="200">Gets the comments.</response>
+    /// <response code="404">Unable to gets the comments.</response>
+    [Get("/api/v1/comments/get-comments-by-filter")]
+    Task<PagedCommentsResponse> GetComments(SortParametersRequest sortParameters = null);
 
-        /// <summary>
-        /// Gets the comments.
-        /// </summary>
-        /// <param name="sortParameters">The sort parameters.</param>
-        /// <returns>Task.</returns>
-        /// <response code="200">Gets the comments.</response>
-        /// <response code="404">Unable to gets the comments.</response>
-        [Get("/api/v1/comments/get-comments-by-filter")]
-        Task<PagedCommentsResponse> GetComments(SortParametersRequest sortParameters = null);
+    /// <summary>
+    /// Gets the comments by post asynchronous.
+    /// </summary>
+    /// <param name="id">The identifier.</param>
+    /// <param name="sortParameters">The sort parameters.</param>
+    /// <returns>Task.</returns>
+    /// <response code="200">Gets the comments by post asynchronous.</response>
+    /// <response code="404">Unable to gets the comments by post.</response>
+    [Get("/api/v1/comments/get-comments-by-post/{id}")]
+    Task<List<CommentResponse>> GetCommentsByPostAsync(int id, SortParametersRequest sortParameters = null);
 
-        /// <summary>
-        /// Gets the comments by post asynchronous.
-        /// </summary>
-        /// <param name="id">The identifier.</param>
-        /// <param name="sortParameters">The sort parameters.</param>
-        /// <returns>Task.</returns>
-        /// <response code="200">Gets the comments by post asynchronous.</response>
-        /// <response code="404">Unable to gets the comments by post.</response>
-        [Get("/api/v1/comments/get-comments-by-post/{id}")]
-        Task<List<CommentResponse>> GetCommentsByPostAsync(int id, SortParametersRequest sortParameters = null);
+    /// <summary>
+    /// Gets the comment.
+    /// </summary>
+    /// <param name="id">The identifier.</param>
+    /// <returns>Task.</returns>
+    /// <response code="200">Gets the comment.</response>
+    /// <response code="404">Unable to gets the comment.</response>
+    [Get("/api/v1/comments/get-comment/{id}")]
+    Task<CommentResponse> GetComment(int id);
 
-        /// <summary>
-        /// Gets the comment.
-        /// </summary>
-        /// <param name="id">The identifier.</param>
-        /// <returns>Task.</returns>
-        /// <response code="200">Gets the comment.</response>
-        /// <response code="404">Unable to gets the comment.</response>
-        [Get("/api/v1/comments/get-comment/{id}")]
-        Task<CommentResponse> GetComment(int id);
+    /// <summary>
+    /// Creates the asynchronous.
+    /// </summary>
+    /// <param name="request">The request.</param>
+    /// <returns>Task.</returns>
+    /// <response code="201">Create the comment.</response>
+    /// <response code="400">Unable to create the comment.</response>
+    [Headers("Authorization: Bearer")]
+    [Post("/api/v1/comments/create")]
+    Task<CommentResponse> CreateAsync(CreateCommentRequest request);
 
-        /// <summary>
-        /// Creates the asynchronous.
-        /// </summary>
-        /// <param name="request">The request.</param>
-        /// <returns>Task.</returns>
-        /// <response code="201">Create the comment.</response>
-        /// <response code="400">Unable to create the comment.</response>
-        [Headers("Authorization: Bearer")]
-        [Post("/api/v1/comments/create")]
-        Task<CommentResponse> CreateAsync(CreateCommentRequest request);
+    /// <summary>
+    /// Edits the asynchronous.
+    /// </summary>
+    /// <param name="id">The identifier.</param>
+    /// <param name="request">The request.</param>
+    /// <returns>Task.</returns>
+    /// <response code="204">Update the comment.</response>
+    /// <response code="400">Unable to update the comment, because model is invalid.</response>
+    /// <response code="404">Unable to update the comment, because comment not found.</response>
+    [Headers("Authorization: Bearer")]
+    [Put("/api/v1/comments")]
+    Task<CommentResponse> EditAsync(int id, UpdateCommentRequest request);
 
-        /// <summary>
-        /// Edits the asynchronous.
-        /// </summary>
-        /// <param name="id">The identifier.</param>
-        /// <param name="request">The request.</param>
-        /// <returns>Task.</returns>
-        /// <response code="204">Update the comment.</response>
-        /// <response code="400">Unable to update the comment, because model is invalid.</response>
-        /// <response code="404">Unable to update the comment, because comment not found.</response>
-        [Headers("Authorization: Bearer")]
-        [Put("/api/v1/comments")]
-        Task<CommentResponse> EditAsync(int id, UpdateCommentRequest request);
-
-        /// <summary>
-        /// Deletes the asynchronous.
-        /// </summary>
-        /// <param name="id">The identifier.</param>
-        /// <returns>Task.</returns>
-        /// <response code="200">Delete the comment.</response>
-        /// <response code="404">Unable to delete the comment, because comment not found.</response>
-        [Headers("Authorization: Bearer")]
-        [Delete("/api/v1/comments/{id}")]
-        Task<CreatedResponse<int>> DeleteAsync(int id);
-    }
+    /// <summary>
+    /// Deletes the asynchronous.
+    /// </summary>
+    /// <param name="id">The identifier.</param>
+    /// <returns>Task.</returns>
+    /// <response code="200">Delete the comment.</response>
+    /// <response code="404">Unable to delete the comment, because comment not found.</response>
+    [Headers("Authorization: Bearer")]
+    [Delete("/api/v1/comments/{id}")]
+    Task<CreatedResponse<int>> DeleteAsync(int id);
 }
