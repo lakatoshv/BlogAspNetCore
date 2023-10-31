6784dc745d7ed9c8eae3d0eab4722fabe1bff01d
Vitalii Lakatosh <some.pochta1@gmail.com>
2023-10-31 20:14:05 +0200

    Rewriting Insert Enumerable function unit tests in TagsServiceTests

--- BlogWebApp/Tests/Blog.ServicesTests/EntityServices/TagsServiceTests.cs
+++ BlogWebApp/Tests/Blog.ServicesTests/EntityServices/TagsServiceTests.cs
@@ -726,17 +726,12 @@ public class TagsServiceTests
     {
         //Arrange
         var random = new Random();
-        var tagId = random.Next(52);
+        var tagId = _fixture.Create<int>();
         var itemsCount = random.Next(10);
-        var newTags = new List<Tag>();
-
-        for (int i = 0; i < itemsCount; i++)
-        {
-            newTags.Add(new Tag
-            {
-                Title = $"Tag {i}",
-            });
-        }
+        var newTags =
+            SetupTagFixture()
+                .CreateMany(itemsCount)
+                .ToList();
 
         _tagsRepositoryMock.Setup(x => x.Insert(newTags))
             .Callback(() =>
@@ -763,17 +758,12 @@ public class TagsServiceTests
     {
         //Arrange
         var random = new Random();
-        var tagId = random.Next(52);
+        var tagId = _fixture.Create<int>();
         var itemsCount = random.Next(10);
-        var newTags = new List<Tag>();
-
-        for (int i = 0; i < itemsCount; i++)
-        {
-            newTags.Add(new Tag
-            {
-                Title = $"Tag {i}",
-            });
-        }
+        var newTags =
+            SetupTagFixture()
+                .CreateMany(itemsCount)
+                .ToList();
 
         _tagsRepositoryMock.Setup(x => x.Insert(newTags))
             .Callback(() =>
