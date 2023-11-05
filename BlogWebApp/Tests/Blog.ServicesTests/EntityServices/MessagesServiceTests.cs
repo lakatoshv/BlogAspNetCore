2974b5733b7f6c3647e7e51d63931a8b82afb568
Vitalii Lakatosh <some.pochta1@gmail.com>
2023-11-05 20:58:32 +0200

    Rewriting Update function unit tests in MessagesServiceTests

--- BlogWebApp/Tests/Blog.ServicesTests/EntityServices/MessagesServiceTests.cs
+++ BlogWebApp/Tests/Blog.ServicesTests/EntityServices/MessagesServiceTests.cs
@@ -995,35 +995,10 @@ public class MessagesServiceTests
     public void Verify_FunctionUpdate_HasBeenCalled(string newMessageSubject)
     {
         //Arrange
-        var random = new Random();
-        var messageId = random.Next(52);
-
-        var sender = new ApplicationUser
-        {
-            Id = new Guid().ToString(),
-            FirstName = "Test fn",
-            LastName = "Test ln",
-            Email = "test@test.test",
-            UserName = "test@test.test"
-        };
-
-        var recipient = new ApplicationUser
-        {
-            Id = new Guid().ToString(),
-            FirstName = $"Test fn{messageId}",
-            LastName = $"Test ln{messageId}",
-            Email = $"test{messageId}@test.test",
-            UserName = $"test{messageId}@test.test"
-        };
-        var newMessage = new Message
-        {
-            SenderId = sender.Id,
-            Sender = sender,
-            RecipientId = recipient.Id,
-            Recipient = recipient,
-            Subject = $"Test subject{messageId}",
-            Body = $"Test body{messageId}"
-        };
+        var messageId = _fixture.Create<int>();
+        var newMessage =
+            SetupMessageFixture()
+                .Create();
 
         _messagesRepositoryMock.Setup(x => x.Insert(newMessage))
             .Callback(() =>
@@ -1053,35 +1028,10 @@ public class MessagesServiceTests
     public void Update_WhenMessageExists_ShouldReturnMessage(string newMessageSubject)
     {
         //Arrange
-        var random = new Random();
-        var messageId = random.Next(52);
-
-        var sender = new ApplicationUser
-        {
-            Id = new Guid().ToString(),
-            FirstName = "Test fn",
-            LastName = "Test ln",
-            Email = "test@test.test",
-            UserName = "test@test.test"
-        };
-
-        var recipient = new ApplicationUser
-        {
-            Id = new Guid().ToString(),
-            FirstName = $"Test fn{messageId}",
-            LastName = $"Test ln{messageId}",
-            Email = $"test{messageId}@test.test",
-            UserName = $"test{messageId}@test.test"
-        };
-        var newMessage = new Message
-        {
-            SenderId = sender.Id,
-            Sender = sender,
-            RecipientId = recipient.Id,
-            Recipient = recipient,
-            Subject = $"Test subject{messageId}",
-            Body = $"Test body{messageId}"
-        };
+        var messageId = _fixture.Create<int>();
+        var newMessage =
+            SetupMessageFixture()
+                .Create();
 
         _messagesRepositoryMock.Setup(x => x.Insert(newMessage))
             .Callback(() =>
