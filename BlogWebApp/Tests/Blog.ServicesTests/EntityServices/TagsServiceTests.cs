d842eeb7ae72877ef386625e28422bfc43e2f57b
Vitalii Lakatosh <some.pochta1@gmail.com>
2023-11-06 09:40:21 +0200

    Rewriting Update function unit tests in TagsServiceTests

--- BlogWebApp/Tests/Blog.ServicesTests/EntityServices/TagsServiceTests.cs
+++ BlogWebApp/Tests/Blog.ServicesTests/EntityServices/TagsServiceTests.cs
@@ -932,12 +932,10 @@ public class TagsServiceTests
     public void Verify_FunctionUpdate_HasBeenCalled(string newTagTitle)
     {
         //Arrange
-        var random = new Random();
-        var tagId = random.Next(52);
-        var newTag = new Tag
-        {
-            Title = $"Ефп {tagId}",
-        };
+        var tagId = _fixture.Create<int>();
+        var newTag =
+            SetupTagFixture()
+                .Create();
 
         _tagsRepositoryMock.Setup(x => x.Insert(newTag))
             .Callback(() =>
@@ -967,12 +965,10 @@ public class TagsServiceTests
     public void Update_WhenCommentExists_ShouldReturnComment(string newTagTitle)
     {
         //Arrange
-        var random = new Random();
-        var tagId = random.Next(52);
-        var newTag = new Tag
-        {
-            Title = $"Tag {tagId}",
-        };
+        var tagId = _fixture.Create<int>();
+        var newTag =
+            SetupTagFixture()
+                .Create();
 
         _tagsRepositoryMock.Setup(x => x.Insert(newTag))
             .Callback(() =>
