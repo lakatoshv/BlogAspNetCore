b661f0e7eaeb0d65d35cb88161ea3bcf8a002426
Vitalii Lakatosh <some.pochta1@gmail.com>
2023-11-14 22:36:57 +0200

    Rewriting Delete By Object function unit tests in MessagesServiceTests

--- BlogWebApp/Tests/Blog.ServicesTests/EntityServices/MessagesServiceTests.cs
+++ BlogWebApp/Tests/Blog.ServicesTests/EntityServices/MessagesServiceTests.cs
@@ -1377,35 +1377,11 @@ public class MessagesServiceTests
     public void Verify_FunctionDeleteByObject_HasBeenCalled()
     {
         //Arrange
-        var random = new Random();
-        var messageId = random.Next(52);
+        var messageId = _fixture.Create<int>();
+        var newMessage =
+            SetupMessageFixture()
+                .Create();
 
-        var sender = new ApplicationUser
-        {
-            Id = new Guid().ToString(),
-            FirstName = "Test fn",
-            LastName = "Test ln",
-            Email = "test@test.test",
-            UserName = "test@test.test"
-        };
-
-        var recipient = new ApplicationUser
-        {
-            Id = new Guid().ToString(),
-            FirstName = $"Test fn{messageId}",
-            LastName = $"Test ln{messageId}",
-            Email = $"test{messageId}@test.test",
-            UserName = $"test{messageId}@test.test"
-        };
-        var newMessage = new Message
-        {
-            SenderId = sender.Id,
-            Sender = sender,
-            RecipientId = recipient.Id,
-            Recipient = recipient,
-            Subject = $"Test subject{messageId}",
-            Body = $"Test body{messageId}"
-        };
         _messagesRepositoryMock.Setup(x => x.Insert(newMessage))
             .Callback(() =>
             {
