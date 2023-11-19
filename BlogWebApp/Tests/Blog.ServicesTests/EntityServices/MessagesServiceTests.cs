0d81dd6ea20d0694063804d341f58b6344cf2f03
Vitalii Lakatosh <some.pochta1@gmail.com>
2023-11-19 11:46:45 +0200

    Rewriting Delete Async By Id function unit tests in MessagesServiceTests

--- BlogWebApp/Tests/Blog.ServicesTests/EntityServices/MessagesServiceTests.cs
+++ BlogWebApp/Tests/Blog.ServicesTests/EntityServices/MessagesServiceTests.cs
@@ -1521,35 +1521,10 @@ public class MessagesServiceTests
     public async Task Verify_FunctionDeleteAsyncById_HasBeenCalled()
     {
         //Arrange
-        var random = new Random();
-        var messageId = random.Next(52);
-
-        var sender = new ApplicationUser
-        {
-            Id = new Guid().ToString(),
-            FirstName = "Test fn",
-            LastName = "Test ln",
-            Email = "test@test.test",
-            UserName = "test@test.test"
-        };
-
-        var recipient = new ApplicationUser
-        {
-            Id = new Guid().ToString(),
-            FirstName = $"Test fn{messageId}",
-            LastName = $"Test ln{messageId}",
-            Email = $"test{messageId}@test.test",
-            UserName = $"test{messageId}@test.test"
-        };
-        var newMessage = new Message
-        {
-            SenderId = sender.Id,
-            Sender = sender,
-            RecipientId = recipient.Id,
-            Recipient = recipient,
-            Subject = $"Test subject{messageId}",
-            Body = $"Test body{messageId}"
-        };
+        var messageId = _fixture.Create<int>();
+        var newMessage =
+            SetupMessageFixture()
+                .Create();
 
         _messagesRepositoryMock.Setup(x => x.InsertAsync(newMessage))
             .Callback(() =>
@@ -1577,35 +1552,10 @@ public class MessagesServiceTests
     public async Task DeleteAsyncById_WhenMessageIsDeleted_ShouldReturnNothing()
     {
         //Arrange
-        var random = new Random();
-        var messageId = random.Next(52);
-
-        var sender = new ApplicationUser
-        {
-            Id = new Guid().ToString(),
-            FirstName = "Test fn",
-            LastName = "Test ln",
-            Email = "test@test.test",
-            UserName = "test@test.test"
-        };
-
-        var recipient = new ApplicationUser
-        {
-            Id = new Guid().ToString(),
-            FirstName = $"Test fn{messageId}",
-            LastName = $"Test ln{messageId}",
-            Email = $"test{messageId}@test.test",
-            UserName = $"test{messageId}@test.test"
-        };
-        var newMessage = new Message
-        {
-            SenderId = sender.Id,
-            Sender = sender,
-            RecipientId = recipient.Id,
-            Recipient = recipient,
-            Subject = $"Test subject{messageId}",
-            Body = $"Test body{messageId}"
-        };
+        var messageId = _fixture.Create<int>();
+        var newMessage =
+            SetupMessageFixture()
+                .Create();
 
         _messagesRepositoryMock.Setup(x => x.InsertAsync(newMessage))
             .Callback(() =>
