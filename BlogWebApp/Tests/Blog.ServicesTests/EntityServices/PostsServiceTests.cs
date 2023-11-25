6c16efb07b458e87f599442593685a35234b2fe4
Vitalii Lakatosh <some.pochta1@gmail.com>
2023-11-26 00:34:11 +0200

    Rewriting Any function unit tests in PostsServiceTests

--- BlogWebApp/Tests/Blog.ServicesTests/EntityServices/PostsServiceTests.cs
+++ BlogWebApp/Tests/Blog.ServicesTests/EntityServices/PostsServiceTests.cs
@@ -1666,19 +1666,10 @@ public class PostsServiceTests
     {
         //Arrange
         var random = new Random();
-        var postsList = new List<Post>();
-
-        for (var i = 0; i < random.Next(100); i++)
-        {
-            postsList.Add(new Post
-            {
-                Id = i,
-                Title = $"Created from ServicesTests {i}",
-                Description = $"Created from ServicesTests {i}",
-                Content = $"Created from ServicesTests {i}",
-                ImageUrl = $"Created from ServicesTests {i}",
-            });
-        }
+        var postsList =
+            SetupPostFixture()
+                .With(x => x.Title, titleSearch)
+                .CreateMany(random.Next(100));
 
         var specification = new PostSpecification(x => x.Title.Contains(titleSearch));
         _postsRepositoryMock.Setup(x => x.Any(specification))
@@ -1703,20 +1694,10 @@ public class PostsServiceTests
         //Test failed
         //Arrange
         var random = new Random();
-        var postsList = new List<Post>();
-
-        for (var i = 0; i < random.Next(100); i++)
-        {
-            postsList.Add(new Post
-            {
-                Id = i,
-                Title = $"Created from ServicesTests {i}",
-                Description = $"Created from ServicesTests {i}",
-                Content = $"Created from ServicesTests {i}",
-                ImageUrl = $"Created from ServicesTests {i}",
-            });
-        }
-
+        var postsList =
+            SetupPostFixture()
+                .With(x => x.Title, titleSearch)
+                .CreateMany(random.Next(100));
 
         var specification = new PostSpecification(x => x.Title.Contains(titleSearch));
         _postsRepositoryMock.Setup(x => x.Any(specification))
@@ -1740,20 +1721,10 @@ public class PostsServiceTests
     {
         //Arrange
         var random = new Random();
-        var postsList = new List<Post>();
-
-        for (var i = 0; i < random.Next(100); i++)
-        {
-            postsList.Add(new Post
-            {
-                Id = i,
-                Title = $"Created from ServicesTests {i}",
-                Description = $"Created from ServicesTests {i}",
-                Content = $"Created from ServicesTests {i}",
-                ImageUrl = $"Created from ServicesTests {i}",
-            });
-        }
-
+        var postsList =
+            SetupPostFixture()
+                .With(x => x.Title, titleSearch)
+                .CreateMany(random.Next(100));
 
         var specification = new PostSpecification(x => x.Title.Equals(titleSearch));
         _postsRepositoryMock.Setup(x => x.Any(specification))
@@ -1777,20 +1748,9 @@ public class PostsServiceTests
     {
         //Arrange
         var random = new Random();
-        var postsList = new List<Post>();
-
-        for (var i = 0; i < random.Next(100); i++)
-        {
-            postsList.Add(new Post
-            {
-                Id = i,
-                Title = $"Created from ServicesTests {i}",
-                Description = $"Created from ServicesTests {i}",
-                Content = $"Created from ServicesTests {i}",
-                ImageUrl = $"Created from ServicesTests {i}",
-            });
-        }
-
+        var postsList =
+            SetupPostFixture()
+                .CreateMany(random.Next(100));
 
         var specification = new PostSpecification(x => x.Title.Equals(titleSearch));
         _postsRepositoryMock.Setup(x => x.Any(specification))
