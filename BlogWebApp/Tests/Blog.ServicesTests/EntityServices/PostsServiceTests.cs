b283c4c1e3aebef15e644b92ba48fa9c51ccd3db
Vitalii Lakatosh <some.pochta1@gmail.com>
2025-02-07 23:19:01 +0200

    Cover posts SearchBySequenceAsync method with unit tests

--- BlogWebApp/Tests/Blog.ServicesTests/EntityServices/PostsServiceTests.cs
+++ BlogWebApp/Tests/Blog.ServicesTests/EntityServices/PostsServiceTests.cs
@@ -1,4 +1,8 @@
-﻿using AutoFixture;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoFixture;
 using AutoFixture.Dsl;
 using AutoMapper;
 using Blog.CommonServices.Interfaces;
@@ -12,10 +16,6 @@ using Blog.EntityServices;
 using Blog.EntityServices.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Blog.ServicesTests.EntityServices;
@@ -2484,9 +2484,63 @@ public class PostsServiceTests
 
     #endregion
 
-    #region NotTestedYet
-    //GenerateQuery(TableFilter tableFilter, string includeProperties = null)
-    //GetMemberName<T, TValue>(Expression<Func<T, TValue>> memberAccess)
+    #region SearchBySequenceAsync function
+
+    /// <summary>
+    /// Search by sequence async.
+    /// When no entities exist should return empty result.
+    /// </summary>
+    [Fact]
+    public async Task SearchBySequenceAsync_WhenNoEntitiesExist_ShouldReturnEmpty()
+    {
+        var data = new List<Post>().AsQueryable();
+        var query = new SearchQuery<Post> { Skip = 0, Take = 5 };
+        var expected = new PagedListResult<Post> { Entities = new List<Post>(), Count = 0 };
+
+        _postsRepositoryMock.Setup(r => r.SearchBySquenceAsync(query, data)).ReturnsAsync(expected);
+
+        var result = await _postsService.SearchBySequenceAsync(query, data);
+
+        Assert.NotNull(result);
+        Assert.Empty(result.Entities);
+    }
+
+    /// <summary>
+    /// Search by sequence async.
+    /// When entities exist and query is null should return paged list.
+    /// </summary>
+    [Fact]
+    public async Task SearchBySequenceAsync_WhenEntitiesExistAndQueryIsNull_ShouldReturnPagedList()
+    {
+        var data = SetupPostFixture().CreateMany(5).AsQueryable();
+        var expected = new PagedListResult<Post> { Entities = data.ToList(), Count = 5 };
+
+        _postsRepositoryMock.Setup(r => r.SearchBySquenceAsync(null, data)).ReturnsAsync(expected);
+
+        var result = await _postsService.SearchBySequenceAsync(null, data);
+
+        Assert.NotNull(result);
+        Assert.Equal(5, result.Entities.Count());
+    }
+
+    /// <summary>
+    /// Search by sequence async.
+    /// When entities exist and sequence is null should return paged list.
+    /// </summary>
+    [Fact]
+    public async Task SearchBySequenceAsync_WhenEntitiesExistAndSequenceIsNullIsNull_ShouldReturnPagedList()
+    {
+        var query = new SearchQuery<Post> { Skip = 0, Take = 5 };
+        var expected = new PagedListResult<Post> { Entities = null, Count = 5 };
+
+        _postsRepositoryMock.Setup(r => r.SearchBySquenceAsync(query, null)).ReturnsAsync(expected);
+
+        var result = await _postsService.SearchBySequenceAsync(query, null);
+
+        Assert.NotNull(result);
+        Assert.Null(result.Entities);
+    }
+
     #endregion
 
     #endregion
