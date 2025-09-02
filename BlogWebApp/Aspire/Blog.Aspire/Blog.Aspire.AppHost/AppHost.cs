f7521a4354245a4aa913b08f74914875d5e016e8
Vitalii Lakatosh <some.pochta1@gmail.com>
2025-09-02 22:14:17 +0300

    Configure BlogRazor in Aspire

--- BlogWebApp/Aspire/Blog.Aspire/Blog.Aspire.AppHost/AppHost.cs
+++ BlogWebApp/Aspire/Blog.Aspire/Blog.Aspire.AppHost/AppHost.cs
@@ -5,6 +5,13 @@ var cache = builder.AddRedis("cache");
 var apiServices = builder.AddProject<Projects.Blog_Web>("BlogWeb");
 //.WithHttpHealthCheck("/health");
 
-
+builder.AddProject<Projects.BlogRazor>("BlogRazor")
+    //.WithLaunchProfile("ShopRazor")
+    .WithExternalHttpEndpoints();
+//.WithHttpHealthCheck("/health");
+//.WithReference(cache)
+//.WaitFor(cache);
+//.WithReference(apiService)
+//.WaitFor(apiService);
 
 builder.Build().Run();
