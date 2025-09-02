9216635d5f4e027eb3d3ce87a35a914294534857
Vitalii Lakatosh <some.pochta1@gmail.com>
2025-09-02 22:13:24 +0300

    Configure BlogWeb in Aspire

--- BlogWebApp/Aspire/Blog.Aspire/Blog.Aspire.AppHost/AppHost.cs
+++ BlogWebApp/Aspire/Blog.Aspire/Blog.Aspire.AppHost/AppHost.cs
@@ -2,6 +2,9 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache");
 
+var apiServices = builder.AddProject<Projects.Blog_Web>("BlogWeb");
+//.WithHttpHealthCheck("/health");
+
 
 
 builder.Build().Run();
