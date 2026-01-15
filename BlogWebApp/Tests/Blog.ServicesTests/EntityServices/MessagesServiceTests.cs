c9f93488ace867802c1aa5a809c76f20c7ed3574
Vitalii Lakatosh <some.pochta1@gmail.com>
2026-01-15 17:48:30 +0200

    Add exception tests case for Search Async in messages service tests

--- BlogWebApp/Tests/Blog.ServicesTests/EntityServices/MessagesServiceTests.cs
+++ BlogWebApp/Tests/Blog.ServicesTests/EntityServices/MessagesServiceTests.cs
@@ -2976,6 +2976,28 @@ public class MessagesServiceTests
         Assert.Null(comments.Entities);
     }
 
+    /// <summary>
+    /// Search async.
+    /// When repository throws exception should throw exception.
+    /// </summary>
+    [Fact]
+    public async Task SearchAsync_WhenRepositoryThrowsException_ShouldThrowException()
+    {
+        // Arrange
+        var query = new SearchQuery<Message>
+        {
+            Skip = 0,
+            Take = 10
+        };
+
+        _messagesRepositoryMock
+            .Setup(r => r.SearchAsync(query))
+            .ThrowsAsync(new Exception("Database error"));
+
+        // Assert
+        await Assert.ThrowsAsync<Exception>(() => _messagesService.SearchAsync(query));
+    }
+
     #endregion
 
     #region SearchBySequenceAsync function
