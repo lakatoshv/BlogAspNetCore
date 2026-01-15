1550a47f3938c841402d1211cea1cc6664535b47
Vitalii Lakatosh <some.pochta1@gmail.com>
2026-01-15 17:47:52 +0200

    Add exception tests case for Search Async in comments service tests

--- BlogWebApp/Tests/Blog.ServicesTests/EntityServices/CommentsServiceTests.cs
+++ BlogWebApp/Tests/Blog.ServicesTests/EntityServices/CommentsServiceTests.cs
@@ -2891,6 +2891,28 @@ public class CommentsServiceTests
         Assert.Null(comments.Entities);
     }
 
+    /// <summary>
+    /// Search async.
+    /// When repository throws exception should throw exception.
+    /// </summary>
+    [Fact]
+    public async Task SearchAsync_WhenRepositoryThrowsException_ShouldThrowException()
+    {
+        // Arrange
+        var query = new SearchQuery<Comment>
+        {
+            Skip = 0,
+            Take = 10
+        };
+
+        _commentsRepositoryMock
+            .Setup(r => r.SearchAsync(query))
+            .ThrowsAsync(new Exception("Database error"));
+
+        // Assert
+        await Assert.ThrowsAsync<Exception>(() => _commentsService.SearchAsync(query));
+    }
+
     #endregion
 
     #region SearchBySequenceAsync function
