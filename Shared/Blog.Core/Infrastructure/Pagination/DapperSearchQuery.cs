ae26bbb3829dd5f992487aab877cb3a9fc97b243
Vitalii Lakatosh <some.pochta1@gmail.com>
2026-02-18 00:39:56 +0200

    Create dapper repository

---
+++ Shared/Blog.Core/Infrastructure/Pagination/DapperSearchQuery.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="DapperSearchQuery.cs" company="Blog">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Blog.Core.Infrastructure.Pagination;
+
+public class DapperSearchQuery
+{
+    /// <summary>
+    /// Gets or sets the message
+    /// </summary>
+    public string? WhereClause { get; set; }
+
+    /// <summary>
+    /// Gets or sets the message.
+    /// </summary>
+    public object? Parameters { get; set; }
+
+    /// <summary>
+    /// Gets or sets order by.
+    /// </summary>
+    public string? OrderBy { get; set; }
+
+    /// <summary>
+    /// Gets or sets skip.
+    /// </summary>
+    public int Skip { get; set; }
+
+    /// <summary>
+    /// Gets or sets take.
+    /// </summary>
+    public int Take { get; set; }
+}
