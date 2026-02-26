ab764a02b9fd8d3461fbac00a21d6ac81b3f0a37
Vitalii Lakatosh <some.pochta1@gmail.com>
2026-02-27 00:19:28 +0200

    Add Startup configure services installers

---
+++ BlogWebApp/BlogMinimalApi/StartupConfigureServicesInstallers/IInstaller.cs
@@ -0,0 +1,17 @@
+﻿namespace BlogMinimalApi.StartupConfigureServicesInstallers;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Installer interface.
+/// </summary>
+public interface IInstaller
+{
+    /// <summary>
+    /// Installs the services.
+    /// </summary>
+    /// <param name="services">The services.</param>
+    /// <param name="configuration">The configuration.</param>
+    void InstallServices(IServiceCollection services, IConfiguration configuration);
+}
