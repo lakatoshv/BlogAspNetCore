8f04486080fa854f698b436cf1cc5f065f4368cd
Vitalii Lakatosh <some.pochta1@gmail.com>
2026-02-27 00:20:46 +0200

    Add Application base installer

---
+++ BlogWebApp/BlogMinimalApi/StartupConfigureServicesInstallers/ApplicationBaseInstaller.cs
@@ -0,0 +1,25 @@
+﻿namespace BlogMinimalApi.StartupConfigureServicesInstallers;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+
+/// <summary>
+/// ApplicationBaseInstaller.
+/// </summary>
+/// <seealso cref="IInstaller" />
+public class ApplicationBaseInstaller : IInstaller
+{
+    /// <inheritdoc cref="IInstaller"/>
+    public void InstallServices(IServiceCollection services, IConfiguration configuration)
+    {
+        // file upload dependency
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+        services.AddAutoMapper(typeof(Program));
+        services.AddOptions();
+        services.AddLocalization();
+        services.AddMemoryCache();
+        services.AddResponseCaching();
+    }
+}
