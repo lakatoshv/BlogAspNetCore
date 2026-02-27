d7c45fbefe151ff92605d7c3f7c21661be2f4781
Vitalii Lakatosh <some.pochta1@gmail.com>
2026-02-28 00:21:14 +0200

    Add Application services installer

---
+++ BlogWebApp/BlogMinimalApi/StartupConfigureServicesInstallers/ApplicationServicesInstaller.cs
@@ -0,0 +1,75 @@
+﻿namespace BlogMinimalApi.StartupConfigureServicesInstallers;
+
+using Blog.Services.Core.Caching;
+using Blog.Services.Core.Caching.Interfaces;
+using Blog.Services.Core.Email.Templates;
+using Blog.Services.Core.Security;
+using Blog.CommonServices;
+using Blog.CommonServices.EmailServices;
+using Blog.CommonServices.EmailServices.Interfaces;
+using Blog.CommonServices.Interfaces;
+using Blog.Core;
+using Blog.Core.Configuration;
+using Blog.Core.Infrastructure;
+using Blog.Core.Interfaces;
+using Blog.Data;
+using Blog.Data.Models;
+using Blog.Data.Repository;
+using Blog.EntityServices.ControllerContext;
+using Blog.EntityServices.EntityFrameworkServices;
+using Blog.EntityServices.EntityFrameworkServices.Identity.Auth;
+using Blog.EntityServices.EntityFrameworkServices.Identity.RefreshToken;
+using Blog.EntityServices.EntityFrameworkServices.Identity.Registration;
+using Blog.EntityServices.EntityFrameworkServices.Identity.User;
+using Blog.EntityServices.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+/// <summary>
+/// Application services installer.
+/// </summary>
+/// <seealso cref="IInstaller" />
+public class ApplicationServicesInstaller : IInstaller
+{
+    /// <inheritdoc cref="IInstaller"/>
+    public void InstallServices(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddTransient(x => x.GetService<IOptions<BlogConfiguration>>()?.Value);
+        services.AddTransient<HostingConfig>();
+
+        services.AddTransient<IStaticCacheManager, MemoryCacheManager>();
+        //services.AddTransient<IDbContext, ApplicationDbContext>();
+        services.AddTransient<IWebHelper, WebHelper>();
+        services.AddTransient<IShareFileProvider, FileProvider>();
+
+        services.AddTransient<IControllerContext, MyControllerContext>();
+        services.AddSingleton<IAuthorizationHandler, HasScopeHandler>();
+        services.AddHttpContextAccessor();
+
+        // Application services
+        services.AddTransient<IUserStore<ApplicationUser>, ApplicationUserStore>();
+        services.AddTransient<IRoleStore<ApplicationRole>, ApplicationRoleStore>();
+        services.AddTransient<IRepository<ApplicationUser>, Repository<ApplicationUser>>();
+
+        services.AddTransient<IAuthService, AuthService>();
+        services.AddTransient<IUserService, UserService>();
+        services.AddTransient<IEmailExtensionService, EmailExtensionService>();
+
+        services.AddTransient<IAuthService, AuthService>();
+        services.AddTransient<IRefreshTokenService, RefreshTokenService>();
+        services.AddTransient<IRegistrationService, RegistrationService>();
+        services.AddTransient<IEmailTemplateProvider, SimpleEmailTemplateProvider>();
+
+        services.AddTransient<IPostsService, PostsService>();
+        services.AddTransient<ICommentsService, CommentsService>();
+        services.AddTransient<IProfileService, ProfileService>();
+        services.AddTransient<IMessagesService, MessagesService>();
+        services.AddTransient<ITagsService, TagsService>();
+        services.AddTransient<IPostsTagsRelationsService, PostsTagsRelationsService>();
+
+        //services.AddTransient<IResponseCacheService, ResponseCacheService>();
+    }
+}
