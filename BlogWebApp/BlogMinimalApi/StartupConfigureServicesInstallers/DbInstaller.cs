287dbeade8315336f78f20945754bde6120a91f6
Vitalii Lakatosh <some.pochta1@gmail.com>
2026-03-01 00:23:02 +0200

    Add Db installer

---
+++ BlogWebApp/BlogMinimalApi/StartupConfigureServicesInstallers/DbInstaller.cs
@@ -0,0 +1,22 @@
+﻿namespace BlogMinimalApi.StartupConfigureServicesInstallers;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Blog.Data;
+using Blog.Data.Extensions;
+
+/// <summary>
+/// Database installer.
+/// </summary>
+/// <seealso cref="IInstaller" />
+public class DbInstaller : IInstaller
+{
+    /// <inheritdoc cref="IInstaller"/>
+    public void InstallServices(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContextPool<ApplicationDbContext>(
+            options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        services.AddUnitOfWork<ApplicationDbContext>();
+    }
+}
