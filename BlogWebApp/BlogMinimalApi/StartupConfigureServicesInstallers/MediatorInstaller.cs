b420e01ed4edf8fe1334c9698c697e6e625026b3
Vitalii Lakatosh <some.pochta1@gmail.com>
2026-03-02 08:56:17 +0200

    Add Mediator installer

---
+++ BlogWebApp/BlogMinimalApi/StartupConfigureServicesInstallers/MediatorInstaller.cs
@@ -0,0 +1,17 @@
+﻿namespace BlogMinimalApi.StartupConfigureServicesInstallers;
+
+using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Blog.Core.Infrastructure.Mediator.Behaviors;
+
+/// <summary>
+/// Mediator installer.
+/// </summary>
+/// <seealso cref="IInstaller" />
+public class MediatorInstaller : IInstaller
+{
+    /// <inheritdoc cref="IInstaller"/>
+    public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        => services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidatorBehavior<,>));
+}
