2b4fef14bb8366e5a6f61d5e89d53039bb4b111b
Vitalii Lakatosh <some.pochta1@gmail.com>
2026-03-02 08:54:16 +0200

    Add Health check installer

---
+++ BlogWebApp/BlogMinimalApi/StartupConfigureServicesInstallers/HealthCheckInstaller.cs
@@ -0,0 +1,30 @@
+﻿namespace BlogMinimalApi.StartupConfigureServicesInstallers;
+
+using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Blog.Data;
+using HealthChecks;
+
+/// <summary>
+/// Health check installer.
+/// </summary>
+/// <seealso cref="IInstaller" />
+public class HealthCheckInstaller : IInstaller
+{
+    public void InstallServices(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddHealthChecks()
+            .AddDbContextCheck<ApplicationDbContext>();
+        // .AddCheck<RedisHealthCheck>("Redis");
+
+        services.Configure<HealthCheckPublisherOptions>(options =>
+        {
+            options.Delay = TimeSpan.FromSeconds(2);
+            options.Predicate = (check) => check.Tags.Contains("ready");
+        });
+
+        services.AddSingleton<IHealthCheckPublisher, ReadinessPublisher>();
+    }
+}
