b68c1abab52a723bac8466863f29ac62da4a310f
Vitalii Lakatosh <some.pochta1@gmail.com>
2026-03-03 09:31:54 +0200

    Add Swagger filter

---
+++ BlogWebApp/BlogMinimalApi/Filters/SwaggerFilters/LowercaseDocumentFilter.cs
@@ -0,0 +1,28 @@
+﻿namespace BlogMinimalApi.Filters.SwaggerFilters;
+
+using Microsoft.OpenApi;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Linq;
+
+/// <summary>
+/// Lowercase routes filter
+/// </summary>
+public class LowercaseDocumentFilter : IDocumentFilter
+{
+    /// <inheritdoc />
+    public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+    {
+        swaggerDoc.Paths = (OpenApiPaths)swaggerDoc.Paths.ToDictionary(entry => LowercaseEverythingButParameters(entry.Key),
+            entry => entry.Value);
+    }
+
+    /// <summary>
+    /// Lowercase everything but parameters.
+    /// </summary>
+    /// <param name="key">The key.</param>
+    /// <returns>string.</returns>
+    private static string LowercaseEverythingButParameters(string key)
+    {
+        return string.Join('/', key.Split('/').Select(x => x.Contains("{") ? x : x.ToLower()));
+    }
+}
---
+++ BlogWebApp/BlogMinimalApi/Filters/SwaggerFilters/SwaggerGroupOperationFilter.cs
@@ -0,0 +1,32 @@
+﻿namespace BlogMinimalApi.Filters.SwaggerFilters;
+
+using Blog.Core.Attributes.SwaggerAttributes;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.OpenApi;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Linq;
+
+/// <summary>
+/// Swagger operation filter for <see cref="SwaggerGroupAttribute"/>
+/// </summary>
+public class SwaggerGroupOperationFilter : IOperationFilter
+{
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        if (context.ApiDescription.ActionDescriptor is not ControllerActionDescriptor controllerActionDescriptor)
+            return;
+
+        var attributes = controllerActionDescriptor.EndpointMetadata
+            .OfType<SwaggerGroupAttribute>()
+            .ToList();
+
+        var tagName = attributes.Count != 0
+            ? attributes.First().GroupName
+            : controllerActionDescriptor.RouteValues["controller"];
+
+        operation.Tags = new HashSet<OpenApiTagReference>
+        {
+            new (tagName)
+        };
+    }
+}
