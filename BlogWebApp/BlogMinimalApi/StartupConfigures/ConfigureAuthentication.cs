9ec7138b90af8b60df3c9fc9a2dd6dfb97e8af34
Vitalii Lakatosh <some.pochta1@gmail.com>
2026-03-04 10:36:19 +0200

    Configure authentication

---
+++ BlogWebApp/BlogMinimalApi/StartupConfigures/ConfigureAuthentication.cs
@@ -0,0 +1,19 @@
+﻿namespace BlogMinimalApi.StartupConfigures;
+
+using Microsoft.AspNetCore.Builder;
+
+/// <summary>
+/// Configure authentication.
+/// </summary>
+public static class ConfigureAuthentication
+{
+    /// <summary>
+    /// Configures the specified application.
+    /// </summary>
+    /// <param name="app">The application.</param>
+    public static void Configure(IApplicationBuilder app)
+    {
+        app.UseCors("EnableCORS");
+        app.UseCors("AllowAllBlazor");
+    }
+}
