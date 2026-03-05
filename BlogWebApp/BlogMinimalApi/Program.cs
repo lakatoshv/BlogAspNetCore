b280967f27ef494353ff370eb16125b0b43a9dc0
Vitalii Lakatosh <some.pochta1@gmail.com>
2026-03-05 10:37:55 +0200

    Configure Program file

--- BlogWebApp/BlogMinimalApi/Program.cs
+++ BlogWebApp/BlogMinimalApi/Program.cs
@@ -1,7 +1,22 @@
+using BlogMinimalApi.ApiEndpoints;
+using BlogMinimalApi.StartupConfigures;
+using BlogMinimalApi.StartupConfigureServicesInstallers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
+// Add services to the container.
+builder.Services.InstallServicesInAssembly(builder.Configuration);
+
 var app = builder.Build();
+
+ConfigureBase.Configure(app, builder.Environment);
+ConfigureSwagger.Configure(app, builder.Configuration);
+ConfigureAuthentication.Configure(app);
+ConfigureRoutes.Configure(app);
+
+//Api endpoints
+app.InstallApiRoutesInAssembly();
 
 app.Run();
