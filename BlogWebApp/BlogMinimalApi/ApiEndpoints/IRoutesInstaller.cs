4ab07b74bf2d2562cb13626c1ef925af6dbb0806
Vitalii Lakatosh <some.pochta1@gmail.com>
2026-03-07 10:41:58 +0200

    Configure Routes

---
+++ BlogWebApp/BlogMinimalApi/ApiEndpoints/IRoutesInstaller.cs
@@ -0,0 +1,13 @@
+﻿namespace BlogMinimalApi.ApiEndpoints;
+
+/// <summary>
+/// Installer interface.
+/// </summary>
+public interface IRoutesInstaller
+{
+    /// <summary>
+    /// Installs the Api routes.
+    /// </summary>
+    /// <param name="app">The app.</param>
+    void InstallApiRoutes(WebApplication app);
+}
---
+++ BlogWebApp/BlogMinimalApi/ApiEndpoints/RouterInstallerExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace BlogMinimalApi.ApiEndpoints;
+
+/// <summary>
+/// Installer extensions.
+/// </summary>
+public static class RouterInstallerExtensions
+{
+    /// <summary>
+    /// Installs the Api routes in assembly.
+    /// </summary>
+    public static void InstallApiRoutesInAssembly(this WebApplication app)
+    {
+        var installers = typeof(Program).Assembly.ExportedTypes.Where(x =>
+                typeof(IRoutesInstaller).IsAssignableFrom(x)
+                && x is { IsInterface: false, IsAbstract: false })
+            .Select(Activator.CreateInstance)
+            .Cast<IRoutesInstaller>().ToList();
+
+        installers.ForEach(installer => installer.InstallApiRoutes(app));
+    }
+}
